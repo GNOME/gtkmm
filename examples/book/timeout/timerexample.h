4ca93e3d05d0a523c52cb3b847a8d361aa924ea1
Murray Cumming <murrayc@src.gnome.org>
2003-04-20 15:57:52 +0000

    Finished last change properly.

--- examples/book/timeout/timerexample.h
+++ examples/book/timeout/timerexample.h
@@ -30,26 +30,30 @@ public:
 
 protected:
   // signal handlers
-  void add_timer_pressed();
-  void del_timer_pressed();
-  // this is the callback function the timeout will call
-  bool on_timer_callback(int timer_nr);
+  void on_button_add_timer();
+  void on_button_delete_timer();
 
-  // member data
-  Gtk::HBox m_box;
-  // Buttons for adding & deleting a timer and quitting the app
-  Gtk::Button m_add_timer, m_del_timer, m_quit;
-  // keep track of the timers being added
+  // This is the callback function the timeout will call
+  bool on_timeout(int timer_number);
+
+  // Member data:
+
+  Gtk::HBox m_Box;
+  Gtk::Button m_ButtonAddTimer, m_ButtonDeleteTimer, m_ButtonQuit;
+
+  // Keep track of the timers being added:
   int m_timer_number;
-  // these two constants are initialized in the member initializer
+
+  // These two constants are initialized in the constructor's member initializer:
   const int count_value;
   const int timeout_value;
+
   // STL map for storing our connections
-  std::map<int,SigC::Connection> m_timers;
-  // STL map for storing our timer values
-  // each timer counts back from COUNT_VALUE to 0 and
-  // is removed when it reaches 0
-  std::map<int,int> m_counters;
+  std::map<int, SigC::Connection> m_timers;
+
+  // STL map for storing our timer values.
+  // Each timer counts back from COUNT_VALUE to 0 and is removed when it reaches 0
+  std::map<int, int> m_counters;
 };
 
 #endif // GTKMM_EXAMPLE_TIMEREXAMPLE_H
