c2200272b853109e4106935d43441da9136cd832
Cedric Gustin <cedric.gustin@swing.be>
2004-11-03 16:20:50 +0000

    Initial release. Added MSVC_Net2003 Makefiles. Added MSVC_Net2003 subdir.
    2004-11-03  Cedric Gustin  <cedric.gustin@swing.be>
    
    	* MSVC_Net2003/*/Makefile.am: Initial release.
    	* configure.in: Added MSVC_Net2003 Makefiles.
    	* Makefile.am: Added MSVC_Net2003 subdir.

--- MSVC_Net2003/gtkmm/gtkmmconfig.h
+++ MSVC_Net2003/gtkmm/gtkmmconfig.h
@@ -7,34 +7,15 @@
 /* version numbers */
 #define GTKMM_MAJOR_VERSION 2
 #define GTKMM_MINOR_VERSION 4
-#define GTKMM_MICRO_VERSION 6
+#define GTKMM_MICRO_VERSION 7
 
-// detect common platforms
-#if defined(_WIN32)
-// Win32 compilers have a lot of varation
-  #if defined(_MSC_VER)
-    #define GTKMM_MSC
-    #define GTKMM_WIN32
-    #define GTKMM_DLL
-  #elif defined(__CYGWIN__)
-    #define GTKMM_CONFIGURE
-  #elif defined(__MINGW32__)
-    #define GTKMM_WIN32
-    #define GTKMM_CONFIGURE
-  #else
-    #warning "Unknown architecture (send me gcc --dumpspecs or equiv)"
-  #endif
-#else
-    #define GTKMM_CONFIGURE
-#endif /* _WIN32 */
-
-#ifdef GTKMM_CONFIGURE
+#ifdef GLIBMM_CONFIGURE
 /* compiler feature tests that are used during compile time and run-time
    by gtkmm only. */
 
-#endif
+#endif /* GLIBMM_CONFIGURE */
 
-#ifdef GTKMM_DLL
+#ifdef GLIBMM_DLL
   #if defined(GTKMM_BUILD) && defined(_WINDLL)
     // Do not dllexport as it is handled by gendef on MSVC
     #define GTKMM_API
@@ -46,7 +27,7 @@
   #endif /* GTKMM_BUILD - _WINDLL */
 #else
   #define GTKMM_API
-#endif /* GTKMM_DLL */
+#endif /* GLIBMM_DLL */
 
 #endif /* _GTKMM_CONFIG_H */
 
