885c4b183fc7b48ee0e01c6590fdcb7981a0baf8
Murray Cumming <murrayc@murrayc.com>
2004-11-16 20:02:21 +0000

    Mention Gtk::manage() in the Basics/ Widgets section, with a link to the
    2004-11-16  Murray Cumming  <murrayc@murrayc.com>
    
            * docs/tutorial/gtkmm-tut.xml: Mention Gtk::manage() in the Basics/
            Widgets section, with a link to the Memory Management chapter, so that
            people have an explanation of manage() before it is used in examples.
            Bug #138248.

--- examples/book/helloworld/helloworld.cc
+++ examples/book/helloworld/helloworld.cc
@@ -26,7 +26,7 @@ HelloWorld::HelloWorld()
   set_border_width(10);
 
   // When the button receives the "clicked" signal, it will call the
-  // hello() method. The hello() method is defined below.
+  // on_button_clicked() method. The on_button_clicked() method is defined below.
   m_button.signal_clicked().connect(sigc::mem_fun(*this, &HelloWorld::on_button_clicked));
 
   // This packs the button into the Window (a container).
