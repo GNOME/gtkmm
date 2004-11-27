289428f9fc30764a63c76183eb1bc05b4aa38676
Chris Vine <cvine@freeserve.co.uk>
2004-11-27 10:44:36 +0000

    This now works.
    2004-11-25  Chris Vine  <cvine@freeserve.co.uk>
    
            * example/book/input/: This now works.

--- examples/book/input/main.cc
+++ examples/book/input/main.cc
@@ -1,50 +1,37 @@
-//$Id$ -*- c++ -*-
-
-/* gtkmm example Copyright (C) 2003 gtkmm development team
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
-
 #include <gtkmm/main.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fstream>
 #include <iostream>
-#include <memory>
 
-std::auto_ptr<std::ifstream> input;
+int read_fd;
 
+/*
+  send to the fifo with:
+  echo "Hello" > testfifo
+
+  quit the program with:
+  echo "Q" > testfifo
+*/
 
 // this will be our signal handler for read operations
-// there is not much to say. just read a string,
-// print it and quit the application if the string was quit
-bool MyCallback(Glib::IOCondition /* condition */)
+// it will print out the message sent to the fifo
+// and quit the program if the message was, or began
+// with, '*'
+bool MyCallback(Glib::IOCondition io_condition)
 {
-  Glib::ustring dummy;
-
-  do
-  {
-    (*input) >> dummy;
-    std::cout << dummy << std::endl;
-  
-    if(dummy == "quit")
-      Gtk::Main::quit();
-
-  } while(input->fail());
-  
+  if ((io_condition & Glib::IO_IN) == 0) {
+    std::cerr << "Invalid fifo response" << std::endl;
+  }
+  else {
+    char buffer[PIPE_BUF];
+    int result;
+    while ((result = read(read_fd, buffer, PIPE_BUF)) > 0) {
+      if (*buffer == 'Q') Gtk::Main::quit();
+      else write(0, buffer, result);
+    }
+  }
   return true;
 }
 
@@ -54,36 +41,23 @@ int main(int argc, char *argv[])
   // the usual Gtk::Main object
   Gtk::Main app(argc, argv);
 
-  // create a fifo for testing purposes
-  if (mkfifo("testfifo", 0666) != 0) {
-    std::cerr << "error creating fifo" << std::endl;
-    return -1;
+  if (access("testfifo", F_OK) == -1) {
+    // fifo doesn't exit - create it
+    if (mkfifo("testfifo", 0666) != 0) {
+      std::cerr << "error creating fifo" << std::endl;
+      return -1;
+    }
   }
-
-  //TODO: I have update this example to build with gtkmm 2, but I'm not sure which of the several commented-out 
-  //ifstream and file descriptor things are needed, or should work.
-  //We need to look at the gtkmm 1.2 version, if it worked.
-  //Alternatively, just find a gtkmm app that monitors files for changes.
-  //murrayc
-  
-  // open the fifo
-  input = std::auto_ptr<std::ifstream>( new std::ifstream("testfifo") );
-  
-  int fd = open("testfifo", 0);
-  if (fd == -1)
+ 
+  read_fd = open("testfifo", O_RDONLY | O_NONBLOCK);
+  if (read_fd == -1)
   {
     std::cerr << "error opening fifo" << std::endl;
     return -1;
   }
 
-  // assign the fifo's filedescriptor to our ifstream object
-  //This sucks; it will only ever work with libstdc++-v3, as
-  //  both istream::__filebuf_type and the basic_filebuf contructor
-  //  that takes an fd are libstdc++-v3 specific.
-  //input = std::auto_ptr<std::ifstream>( new std::istream(new std::ifstream::__filebuf_type(fd, "testfifo")) );
-
   // connect the signal handler
-  Glib::signal_io().connect(sigc::ptr_fun(MyCallback), fd, Glib::IO_IN);
+  Glib::signal_io().connect(sigc::ptr_fun(MyCallback), read_fd, Glib::IO_IN);
 
   // and last but not least - run the application main loop
   app.run();
