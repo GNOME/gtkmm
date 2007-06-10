7c547a273b1a80dd9eedc0061b68a0d7bf766c8c
Kalle Vahlman <zuh@iki.fi>
2007-06-10 10:37:42 +0000

    #ifdef out the use of the regular property API if properties are disabled.
    2007-06-10  Kalle Vahlman  <zuh@iki.fi>
    
            * tests/property_notification/main.cc: #ifdef out the use of the regular property API 
            if properties are disabled.
    
    svn path=/branches/gtkmm-2-10/; revision=856

--- tests/property_notification/main.cc
+++ tests/property_notification/main.cc
@@ -27,7 +27,10 @@ int main (int argc, char **argv)
   button.show();
 
   button.connect_property_changed("color", sigc::ptr_fun(&on_property_color_changed));
+
+#ifdef GLIBMM_PROPERTIES_ENABLED
   button.property_color().signal_changed().connect(sigc::ptr_fun(&on_property_color_changed_nicer_api));
+#endif
 
   button.connect_property_changed("name", sigc::ptr_fun(&on_property_name_changed));
 
