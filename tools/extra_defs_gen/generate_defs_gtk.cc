d7f06a452288057d178cbdba4f101672539e71de
Deng Xiyue <manphiz@gmail.com>
2009-01-19 17:55:19 +0000

    Change license header to mention Lesser General Public License version 2.1
    2009-01-20  Deng Xiyue  <manphiz@gmail.com>
    
    * Change license header to mention Lesser General Public License
    version 2.1 instead of Library General Public License, to be
    consistent with COPYING.
    
    svn path=/trunk/; revision=1082

--- tools/extra_defs_gen/generate_defs_gtk.cc
+++ tools/extra_defs_gen/generate_defs_gtk.cc
@@ -12,9 +12,9 @@
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public
+ * You should have received a copy of the GNU General Public
  * License along with this library; if not, write to the Free
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
