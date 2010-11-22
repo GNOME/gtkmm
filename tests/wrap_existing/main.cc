04db85945ed4c6bf3132838e28e1638c35dc6d60
Murray Cumming <murrayc@murrayc.com>
2010-11-22 15:45:51 +0100

    2.91.4

--- tests/wrap_existing/main.cc
+++ tests/wrap_existing/main.cc
@@ -23,7 +23,7 @@ int main(int argc, char**argv)
   Gtk::Main app(&argc, &argv);
 
   Gtk::Dialog* pDialog = new Gtk::Dialog();
-  Gtk::VBox* pBox = pDialog->get_vbox();
+  Gtk::Box* pBox = pDialog->get_vbox();
 
   //Set a quark and a callback:
   initialize_quark();
