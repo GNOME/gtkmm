b2a3e8bf99fcc4679ccb907172f605747fe2277c
Kjell Ahlstedt <kjell.ahlstedt@bredband.net>
2013-07-02 19:29:07 +0200

    Deprecate Deprecate Gtk::IconFactory, IconSet, IconSource.
    * gtk/gtkmm/wrap_init.h: Add GDK_DISABLE_DEPRECATION_WARNINGS, which is now
    necessary in wrap_init.cc.
    * gtk/src/iconfactory.ccg: Move IconSize code to enums.ccg.
    * gtk/src/iconfactory.hg: Deprecate IconFactory.
    * gtk/src/iconset.hg: Deprecate IconSet.
    * gtk/src/iconsource.hg: Deprecate IconSource.
    * gtk/src/enums.ccg: Move IconSize code from iconfactory.ccg.
    * gtk/src/enums.hg: Deprecate most IconSize methods.
    * gtk/src/image.[hg|ccg]: Deprecate methods that take an IconSet or a StockID
    parameter.
    * gtk/src/stylecontext.hg: Deprecate [set|get]_direction() and
    render_icon_pixbuf().

--- gtk/gtkmm/wrap_init.h
+++ gtk/gtkmm/wrap_init.h
@@ -25,6 +25,14 @@
 namespace Gtk
 {
 
+// This is needed in wrap_init.cc, but to get it there in a better way than
+// putting it here requires a change in generate_wrap_init.pl.
+// It's required because some gtk_*_get_type() functions are deprecated, but
+// their .h files have not been moved to the gtk+/gtk/deprecated directory.
+#ifndef GTKMM_DISABLE_DEPRECATED
+#define GDK_DISABLE_DEPRECATION_WARNINGS 1
+#endif
+
 void wrap_init();
 
 } /* namespace Gtk */
