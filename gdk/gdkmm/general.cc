8efedb8765851cd4c7bad9ad5268b4505c8167b2
Kjell Ahlstedt <kjell.ahlstedt@bredband.net>
2015-12-10 10:53:23 +0100

    Gdk: Use nullptr instead of 0

--- gdk/gdkmm/general.cc
+++ gdk/gdkmm/general.cc
@@ -1,6 +1,3 @@
-// -*- c++ -*-
-/* $Id$ */
-
 /* Copyright 2002      The gtkmm Development Team
  *
  * This library is free software; you can redistribute it and/or
@@ -87,7 +84,7 @@ void add_rectangle_to_path(const ::Cairo::RefPtr< ::Cairo::Context >& context, c
 
 void add_region_to_path(const ::Cairo::RefPtr< ::Cairo::Context >& context, const ::Cairo::RefPtr< ::Cairo::Region>& region)
 {
-  gdk_cairo_region(context->cobj(), (region ? region->cobj() : 0));
+  gdk_cairo_region(context->cobj(), (region ? region->cobj() : nullptr));
 }
 
 } //namespace Cairo
