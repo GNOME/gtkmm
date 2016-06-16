38917eecc95378bbbfe7b14801426fdad0672a1c
Kjell Ahlstedt <kjell.ahlstedt@bredband.net>
2016-06-16 18:25:34 +0200

    Add Gdk::DrawingContext
    * .gitignore: Add gdk/gdkmm/drawingcontext.[h|cc].
    * gdk/gdkmm.h: Add drawingcontext.h.
    * gdk/src/filelist.am: Add drawningcontext.hg.
    * gdk/src/drawingcontext.[hg|ccg]: New files.
    * tools/m4/convert_gdk.m4: Add conversions for Gdk::DrawingContext.

--- gdk/gdkmm.h
+++ gdk/gdkmm.h
@@ -30,6 +30,7 @@
 #include <gdkmm/pixbufloader.h>
 #include <gdkmm/applaunchcontext.h>
 #include <gdkmm/cursor.h>
+#include <gdkmm/drawingcontext.h>
 #include <gdkmm/glcontext.h>
 #include <gdkmm/rectangle.h>
 #include <gdkmm/display.h>
