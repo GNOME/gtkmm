78ca0107a1f37113a2a2116cb602158d6736881e
Marcin Kolny <marcin.kolny@gmail.com>
2016-11-06 19:07:12 +0100

    mmgirgenerator: add gen script, update enums definitions

---
+++ tools/gen_scripts/gtkmm_generate_defs.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ -z "$GTKMM_DIR" ]; then
+  echo -e "GTKMM_DIR must contain the path to the gtkmm repository."
+  exit 1;
+fi
+
+function patch_directory {
+    PATCH_OPTIONS="--backup --version-control=simple --suffix=.orig"
+    for patch_file in $1/*.patch;
+    do
+	orig_file=$(echo $patch_file | sed s/.patch$//)
+	patch $PATCH_OPTIONS $orig_file $patch_file
+    done
+}
+
+MMGIRGEN_PATH=mmgirgenerator
+
+############## Gdk ###############
+# Gdk enums
+$MMGIRGEN_PATH Gdk-3.0 \
+    --namespace=Gdk \
+    --print-enum > $GTKMM_DIR/gdk/src/gdk_enums.defs
+
+patch_directory $GTKMM_DIR/gdk/src
+
+
+############## Gtk ###############
+# Gtk enums
+$MMGIRGEN_PATH Gtk-3.0 \
+    --namespace=Gtk \
+    --print-enum > $GTKMM_DIR/gtk/src/gtk_enums.defs
+
+patch_directory $GTKMM_DIR/gtk/src
