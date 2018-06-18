b2b3afd0eaf25296fd0fa0df0653b8dd43145ca9
Kjell Ahlstedt <kjellahlstedt@gmail.com>
2018-06-18 15:48:00 +0200

    Add Gtk::Picture and Gtk::WidgetPaintable

--- gtk/gtkmm.h
+++ gtk/gtkmm.h
@@ -204,6 +204,7 @@ extern const int gtkmm_micro_version;
 # include <gtkmm/pagesetupunixdialog.h>
 #endif
 #include <gtkmm/papersize.h>
+#include <gtkmm/picture.h>
 #include <gtkmm/popover.h>
 #include <gtkmm/popovermenu.h>
 #include <gtkmm/printcontext.h>
@@ -276,6 +277,7 @@ extern const int gtkmm_micro_version;
 #include <gtkmm/viewport.h>
 #include <gtkmm/volumebutton.h>
 #include <gtkmm/widget.h>
+#include <gtkmm/widgetpaintable.h>
 #include <gtkmm/widgetpath.h>
 #include <gtkmm/window.h>
 
